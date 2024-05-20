eff781137c3d7f96de429d608bcdf6048e3cb3ed
Dayeon-Cheon <dayeon.cheonn@gmail.com>
2024-05-20 15:52:10 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
+import { useState } from "react";
+
 export default function App() {
+  const [originNum, setOriginNum] = useState(0);
+  const [newNum, setNewNum] = useState(0);
+  const [resultNum, setResultNum] = useState(0);
+
+  const addNum = () => {
+    console.log(e.target.value);
+    setOriginNum(originNum - newNum);
+  };
+
+  const subtractNum = () => {};
+
+  const resetNum = () => {};
+
   return (
     <div>
       <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
       <div>
-        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
-        <button>초기화</button>
+        <input type="text" /> 만큼을 <button onClick={addNum}>더할게요</button>{" "}
+        <button onClick={subtractNum}>뺄게요</button>
+        <button onClick={resetNum}>초기화</button>
       </div>
       <hr />
       <div>
         <h3>결과</h3>
-        <p>0</p>
+        <p>{resultNum}</p>
       </div>
     </div>
   );
