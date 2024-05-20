68a0f78159e9818a8d06c52ef35cb01039906448
Dayeon-Cheon <dayeon.cheonn@gmail.com>
2024-05-20 18:25:14 +0900

    feat add and subtract

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,37 @@ import { useState } from "react";
 
 export default function App() {
   const [originNum, setOriginNum] = useState(0);
-  const [newNum, setNewNum] = useState(0);
+  const [inputNum, setInputNum] = useState(0);
   const [resultNum, setResultNum] = useState(0);
 
-  const addNum = () => {
-    console.log(e.target.value);
-    setOriginNum(originNum - newNum);
+  const onChangeNum = (e) => {
+    setInputNum(Number(e.target.value));
   };
 
-  const subtractNum = () => {};
+  const addNum = () => {
+    const newResult = originNum + inputNum;
+    setResultNum(newResult);
+    setOriginNum(newResult);
+  };
 
-  const resetNum = () => {};
+  const subtractNum = () => {
+    const newResult = originNum - inputNum;
+    setResultNum(newResult);
+    setOriginNum(newResult);
+  };
+
+  const resetNum = () => {
+    setOriginNum(0);
+    setInputNum(0);
+    setResultNum(0);
+  };
 
   return (
     <div>
       <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
       <div>
-        <input type="text" /> 만큼을 <button onClick={addNum}>더할게요</button>{" "}
+        <input onChange={onChangeNum} value={inputNum} type="text" /> 만큼을{" "}
+        <button onClick={addNum}>더할게요</button>{" "}
         <button onClick={subtractNum}>뺄게요</button>
         <button onClick={resetNum}>초기화</button>
       </div>
